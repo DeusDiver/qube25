6e7e30d0b1ba5c7927366a8628440509da5af9e3
Oliver <olivers.gundersen@gmail.com>
2025-03-18 11:53:29 +0100

    Sletta test, oppdatert setup, manuelt satt half box disk_z

--- src/qube_description/setup.py
+++ src/qube_description/setup.py
@@ -1,25 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'qube_description'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf', glob('urdf/*.urdf')),
+        ('share/' + package_name + '/urdf', glob('urdf/*.xacro')),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*.rviz')),  # Legger til config-filer
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='oliver',
     maintainer_email='olivers.gundersen@gmail.com',
-    description='TODO: Package description',
+    description='ROS2 package for visualizing robot joints in RViz',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
